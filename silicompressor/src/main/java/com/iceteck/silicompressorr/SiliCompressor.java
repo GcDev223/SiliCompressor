c01126d4c20bc89f0763fd33a2cf467fdafe5c91
GcDev223 <gcdev223@gmail.com>
2023-03-15 17:07:45 +0500

    Custom Quality MAX H/W

--- silicompressor/src/main/java/com/iceteck/silicompressorr/SiliCompressor.java
+++ silicompressor/src/main/java/com/iceteck/silicompressorr/SiliCompressor.java
@@ -292,8 +292,8 @@ public class SiliCompressor {
             int actualWidth = options.outWidth;
 
             // max Height and width values of the compressed image is taken as 816x612
-            float maxHeight = 816.0f;
-            float maxWidth = 612.0f;
+            float maxHeight = 1280.0f;
+            float maxWidth = 720.0f;
             float imgRatio = actualWidth / actualHeight;
             float maxRatio = maxWidth / maxHeight;
 
